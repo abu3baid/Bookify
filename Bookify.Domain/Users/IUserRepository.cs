9c73d7e44b0e435c3134114c72a9df5ef6d34e53
Abu3baid <m76eef@gmail.com>
2024-02-20 11:12:09 +0200

    Init

---
+++ Bookify.Domain/Users/IUserRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Bookify.Domain.Users;
+
+public interface IUserRepository
+{
+    Task<User?> GetByUserId (Guid userId, CancellationToken cancellationToken = default);
+    void Add(User user);
+}
